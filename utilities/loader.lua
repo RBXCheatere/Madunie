bb6c6ce82d5f799fecf8ff7eed2415b92f473f92
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2022-03-11 10:08:50 -0500

    Update loader.lua

--- utilities/loader.lua
+++ utilities/loader.lua
@@ -23,11 +23,7 @@ local Loader = Library:Create("ScreenGui", {
     Enabled = false
 })
 
-if syn.protect_gui then
-    syn.protect_gui(Loader)
-elseif get_hidden_gui then
-    get_hidden_gui(Loader)
-end
+
 
 if gethui then
     Loader.Parent = gethui()
